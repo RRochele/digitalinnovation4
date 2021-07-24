9a21b22db83e1dd1712ba05988a083d7de4dee32
RRochele <rrochelefsd@gmail.com>
2021-07-24 15:10:22 -0300

    created person class

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,14 +3,18 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String= "Jether"
     var cpf: String = "123.123.123-11"
+
+    inner class Endereco {
+        var rua: String = "rua teste"
+    }
 }
 
 fun main() {
     val jether= Pessoa()
-    //criando um atributo que recebe a instancia de uma pessoa e quando ela recebe esta instancia esta atribuido na variavel jether uma referencia na memoria que vai ter esse estado : nome jether mais cpf
 
     println(jether.nome)
     println(jether.cpf)
-    //consigo representar dentro do dominio da nossa aplicação da interface
+    println(jether.Endereco().rua)
+
 }
 
