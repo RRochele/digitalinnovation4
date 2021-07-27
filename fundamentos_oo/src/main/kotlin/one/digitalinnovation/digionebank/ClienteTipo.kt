a0197fb5fd1c91de34c6be1f5f22951ded97d299
RRochele <rrochelefsd@gmail.com>
2021-07-27 20:23:30 -0300

    criated enum client type and test to it

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/ClienteTipo.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/ClienteTipo.kt
@@ -3,4 +3,6 @@ package one.digitalinnovation.digionebank
 enum class ClienteTipo(val descricao: String) {
     PF("Pessoa Física"),
     PJ("Pessoa Jurídica");
+
+
 }
--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteClienteTipoEnum.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteClienteTipoEnum.kt
@@ -2,18 +2,24 @@ package one.digitalinnovation.digionebank.testes
 
 import one.digitalinnovation.digionebank.ClienteTipo
 
+
 fun main() {
     ClienteTipo.values().forEach { elemento ->
         println("${elemento.name} - ${elemento.descricao}")
     }
 
-    ClienteTipo.values().forEach {
+    ClienteTipo.values().forEach{
         println("${it.name} - ${it.descricao}")
     }
 
-    val pf = ClienteTipo.PF
-    println(">> ${pf.name} - ${pf.descricao}")
+    val pf= ClienteTipo.PF
+    println(">>${pf.name} - ${pf.descricao}")
 
-    val pj = ClienteTipo.PJ
-    println(">> ${pj.name} - ${pj.descricao}")
+    val pj= ClienteTipo.PJ
+    println(">>${pj.name} - ${pj.descricao}")
+
 }
+
+
+
+
