9037a8a80a436e15a64cca6a3bd4cca913da6045
RRochele <rrochelefsd@gmail.com>
2021-07-27 21:14:27 -0300

    criated person test

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Teste/TestePessoa.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.digionebank.Teste
+
+import one.digitalinnovation.digionebank.Pessoa
+
+    fun main() {
+        val jether = Pessoa( nome= "Jether Rodrigues", cpf="123.123.123-11")
+        println(jether.nome)
+        println(jether.cpf)
+    }
+
+
