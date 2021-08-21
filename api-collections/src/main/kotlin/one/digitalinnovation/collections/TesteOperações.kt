ccfde55bcf97ca38b2317ea17292ef08a2b55f47
RRochele <rrochelefsd@gmail.com>
2021-08-21 13:09:56 -0300

    Created collection operations

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperações.kt
@@ -0,0 +1,28 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios= doubleArrayOf(1000.0, 2250.0, 4000.0)
+    //iterar
+    for (salario in salarios) {
+        println(salario)
+
+    }
+    println("--------------------------")
+    println("Maior salario: ${salarios.maxOrNull()}")
+    println("Menor salario: ${salarios.minOrNull()}")
+    println("Media salario: ${salarios.average()}")
+
+
+    val salariosMaiorQue2500 = salarios.filter{it > 2500.0}
+    println("--------------------------")
+    salariosMaiorQue2500.forEach{println(it)}
+
+    println("--------------------------")
+    println(salarios.find { it == 2250.00 })
+    println(salarios.find { it ==500.0 })
+
+    println("--------------------------")
+    println(salarios.any { it == 1000.0 })
+    println(salarios.any { it == 500.0 })
+
+}
